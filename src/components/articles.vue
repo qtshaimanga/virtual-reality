7891792103692d38c72fa054cf90c835ff24bcf2
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-03-13 16:01:32 +0100

    sass and first step for request with params

--- src/components/articles.vue
+++ src/components/articles.vue
@@ -1,4 +1,6 @@
 <template>
+ <button v-on:click="selectedTags" name="yolo">yolo</button>
+ <button v-on:click="selectedTags" name="yolo-art">yolo-art</button>
   <div class="articles">
     <div id="article" v-for="article in articlesList.data">
       <h5>{{ article.date }}</h5>
@@ -9,8 +11,8 @@
 </template>
 
 
-<script>
 
+<script>
 export default {
   data: {
      articlesList: ''
@@ -18,29 +20,45 @@ export default {
 
   ready: function() {
 
-      this.$http({url:'/api/articles', method: 'GET'}).then(function (response) {
+      this.$http({url:'./articles', method: 'GET'}).then(function (response) {
+          this.$set('articlesList', response)
+      }, function (response) {
+          console.log("bad request /api/articles");
+      });
+
+  },
+  methods: {
+    selectedTags: function (event) {
+      console.log(String(event.target.name))
+
+
+      this.$http({url:'./articles/', method:'GET', params:''}).then(function (response) {
           this.$set('articlesList', response)
       }, function (response) {
           console.log("bad request /api/articles");
       });
 
     }
-}
+  }
 
+  // function selec articlesTags with Tags
+
+
+}
 </script>
 
 
-<style>
+<style lang="sass">
+$blue: #3bbfce;
 
 .articles{
   color: green;
 }
 
 #article{
-  color: red;
+  color: $blue;
   border-style: solid;
   border-color: black;
   margin: 1em;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.use(require('vue-resource'));
-Vue.http.options.root = 'http://localhost:9000/';
+Vue.http.options.root = 'http://localhost:9000/api';
 
 /* eslint-disable no-new */
 new Vue({
