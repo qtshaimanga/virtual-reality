462abb87ca53b9921896ee63dd2fb099b24debcc
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-03-13 17:07:33 +0100

    button tags to sort items

--- src/components/articles.vue
+++ src/components/articles.vue
@@ -1,6 +1,7 @@
 <template>
  <button v-on:click="selectedTags" name="yolo">yolo</button>
- <button v-on:click="selectedTags" name="yolo-art">yolo-art</button>
+ <button v-on:click="selectedTags" name="art">art</button>
+
   <div class="articles">
     <div id="article" v-for="article in articlesList.data">
       <h5>{{ article.date }}</h5>
@@ -29,10 +30,9 @@ export default {
   },
   methods: {
     selectedTags: function (event) {
-      console.log(String(event.target.name))
 
-
-      this.$http({url:'./articles/', method:'GET', params:''}).then(function (response) {
+      var tag = String(event.target.name)
+      this.$http({url:'./articles/'+ tag, method:'GET'}).then(function (response) {
           this.$set('articlesList', response)
       }, function (response) {
           console.log("bad request /api/articles");
@@ -40,9 +40,6 @@ export default {
 
     }
   }
-
-  // function selec articlesTags with Tags
-
 
 }
 </script>
